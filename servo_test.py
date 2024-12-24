0f13e35f9194481a9079dac50e1209aeeb6d8210
Sal03-git <salah003.edu@gmail.com>
2024-12-24 13:54:14 +0200

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -1,42 +1,41 @@
 from adafruit_servokit import ServoKit
 import time
 
-# Initialize the ServoKit for a 16-channel PCA9685
+# Initialize PCA9685 with 16 channels
 kit = ServoKit(channels=16)
 
-# Function to test servos
-def test_servos():
-    print("Testing servo movement...")
+def test_two_servos():
+    print("Starting servo test for 2 servos...")
     while True:
         try:
-            # Move servo on channel 0
-            print("Moving servo on channel 0 to 0 degrees")
+            # Move Servo 1 (Channel 0) to different angles
+            print("Moving Servo 1 (Channel 0) to 0°")
             kit.servo[0].angle = 0
             time.sleep(1)
 
-            print("Moving servo on channel 0 to 90 degrees")
+            print("Moving Servo 1 (Channel 0) to 90°")
             kit.servo[0].angle = 90
             time.sleep(1)
 
-            print("Moving servo on channel 0 to 180 degrees")
+            print("Moving Servo 1 (Channel 0) to 180°")
             kit.servo[0].angle = 180
             time.sleep(1)
 
-            # Move servo on channel 1
-            print("Moving servo on channel 1 to 0 degrees")
+            # Move Servo 2 (Channel 1) to different angles
+            print("Moving Servo 2 (Channel 1) to 0°")
             kit.servo[1].angle = 0
             time.sleep(1)
 
-            print("Moving servo on channel 1 to 90 degrees")
+            print("Moving Servo 2 (Channel 1) to 90°")
             kit.servo[1].angle = 90
             time.sleep(1)
 
-            print("Moving servo on channel 1 to 180 degrees")
+            print("Moving Servo 2 (Channel 1) to 180°")
             kit.servo[1].angle = 180
             time.sleep(1)
         except KeyboardInterrupt:
-            print("\nStopping servo test.")
+            print("\nServo test stopped.")
             break
 
-# Run the test function
-test_servos()
+# Run the servo test function
+test_two_servos()
